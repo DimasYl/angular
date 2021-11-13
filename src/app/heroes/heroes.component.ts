6bcca601829f4c2d77a17bf9f94c027dbbfedcbb
DimasYl <dima.ilyushin.2016@mail.ru>
2021-11-13 17:50:47 +0300

    ngFor && ngIf

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
 
 @Component({
@@ -7,14 +8,18 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+
+
+  public heroes: Hero[] = HEROES
+  public selectedHero?: Hero
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 
 }
